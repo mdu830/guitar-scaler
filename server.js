4b89acfb44b0eb7f11ad09ca562ab1dcfd4c2426
mdu830 <mdu830@yahoo.com>
2021-10-23 14:43:19 -0400

    created basic react app with server and routes

---
+++ server.js
@@ -0,0 +1,19 @@
+if (process.env.NODE_ENV !== 'production') {
+    console.log('loading dev environments');
+
+}
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const routes = require('./routes');
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+}
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`);
+});
