2909b0dd86454270811568075aaa6d41a075d8af
mdu830 <mdu830@yahoo.com>
2021-11-11 16:37:22 -0500

    created welcomePage component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <welcomePage msg="The best place to visualize your scales and advance your skills"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import welcomePage from './components/welcomePage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    welcomePage
   }
 }
 </script>
@@ -22,5 +21,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+background: linear-gradient(
+  90deg,
+  rgb(54, 54, 66) 1%, 
+  rgba(7,7,28) 8%, 
+  rgba(7,7,28) 92%, 
+  rgb(54, 54, 66) 98%);
 }
 </style>
