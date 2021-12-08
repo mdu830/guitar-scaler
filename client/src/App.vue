25683d5628cdfca70632b25bf132697e85af2b08
mdu830 <mdu830@yahoo.com>
2021-12-08 14:26:27 -0500

    getting basic components built

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/guitar-scaler">Guitar-Scaler</router-link>
   </div>
   <router-view/>
 </template>
---
+++ client/src/views/guitarScaler.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="guitar-scaler">
+
+  </div>
+</template>
