5d192352b3ae01ae0b181321c2f815a11f14bcd9
mdu830 <mdu830@yahoo.com>
2022-02-07 16:48:31 -0500

    added working slider to adjust thru keys, rearranged file organization

--- client/src/components/scaler/scalerContainer.js
+++ client/src/components/scaler/scalerContainer.js
@@ -1,108 +1,49 @@
 import './style.css'
 import { useState } from 'react';
-import { Button, Navbar } from 'reactstrap';
-// Scales
 import ScalerGrid from './scalerGrid';
-import EScaleData from './scales/E/EScale'
-import FScaleData from './scales/F/FScale';
-import FSharpScaleData from './scales/F#/F#Scale';
-import GScaleData from './scales/G/GScale';
-import GSharpScaleData from './scales/G#/G#Scale';
-import AScaleData from './scales/A/AScale';
-import ASharpScaleData from './scales/A#/A#Scale';
-import BScaleData from './scales/B/BScale';
-import CScaleData from './scales/C/CScale';
-import CSharpScaleData from './scales/C#/C#Scale';
-import DScaleData from './scales/D/DScale';
-import DSharpScaleData from './scales/D#/D#Scale';
+import marks from './scales/pentatonic/mark';
+import { Slider } from '@mui/material';
+import { Box } from '@mui/system';
+
+function valueLabelFormat(value) {
+    return `key of ${marks[value].label}`;
+  }
 
 
 export default function GuitarScaler() {
+    
+    const [value, setValue] = useState(0);
+    const [scale, setScale] = useState(marks[0].scale)
 
-    const [scale, setScale] = useState(EScaleData)
-
-    const marks = [
-        {
-            value: 0,
-            label: 'Em',
-            scale: EScaleData,
-        },
-        {
-            value: 1,
-            label: 'F',
-            scale: FScaleData,
-        },
-        {
-            value: 2,
-            label: 'F#',
-            scale: FSharpScaleData,
-        },
-        {
-            value: 3,
-            label: 'G',
-            scale: GScaleData,
-        },
-        {
-            value: 4,
-            label: 'G#',
-            scale: GSharpScaleData,
-        },
-        {
-            value: 5,
-            label: 'A',
-            scale: AScaleData,
-        },
-        {
-            value: 6,
-            label: 'A#',
-            scale: ASharpScaleData,
-        },
-        {
-            value: 7,
-            label: 'B',
-            scale: BScaleData,
-        },
-        {
-            value: 8,
-            label: 'C',
-            scale: CScaleData,
-        },
-        {
-            value: 9,
-            label: 'C#',
-            scale: CSharpScaleData,
-        },
-        {
-            value: 10,
-            label: 'D',
-            scale: DScaleData,
-        },
-        {
-            value: 11,
-            label: 'D#',
-            scale: DSharpScaleData,
+    const handleChange = (event, newValue) => {
+        if (typeof newValue === 'number') {
+          setValue(newValue);
+          setScale(marks[newValue].scale)
         }
+      };
 
-    ];
-
-    const handleDownClick = () => {
-        setScale(marks[10].scale)
-    }
-
-    const handleUpClick = () => {
-        setScale(marks[11].scale)
-    }
 
     return (
         <div id="application">
             <div id='scaler'>
                 <ScalerGrid data={scale} />
             </div>
-            <Navbar className="titleBar" color="dark" dark>
-                <Button onClick={handleDownClick}>Down</Button>
-                <Button onClick={handleUpClick}>Up</Button>
-            </Navbar>
+            <div className="controlBar" color="dark">
+                <Box sx={{ width: 'auto', padding: '25px'}}>
+                    <Slider
+                        value={value}
+                        min={0}
+                        step={1}
+                        max={11}
+                        getAriaValueText={valueLabelFormat}
+                        valueLabelFormat={valueLabelFormat}
+                        onChange={handleChange}
+                        valueLabelDisplay="auto"
+                        aria-labelledby="non-linear-slider"
+                    />
+                </Box>
+            </div>
 
-        </div>
+        </div >
     );
 }
