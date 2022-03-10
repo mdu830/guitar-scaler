e930359199af7960700fe1ed6664f64a4dc43d41
mdu830 <mdu830@yahoo.com>
2022-03-10 15:56:30 -0500

    renamed scale data files and updated marks

--- client/src/components/scaler/scales/pentatonic/mark.js
+++ client/src/components/scaler/scales/pentatonic/mark.js
@@ -1,16 +1,16 @@
 // Scales
-import EScaleData from './scales/EScaleNew'
-import FScaleData from './scales/FScaleNew';
-import FSharpScale from './scales/F#ScaleNew'
-import GScaleData from './scales/GScaleNew';
-import GSharpScale from './scales/G#ScaleNew';
-import AScaleData from './scales/AScaleNew';
-import ASharpScale from './scales/A#ScaleNew';
-import BScaleData from './scales/BScaleNew';
-import CScaleData from './scales/CScaleNew';
-import CSharpScale from './scales/C#ScaleNew';
-import DScaleData from './scales/DScaleNew';
-import DSharpScale from './scales/D#ScaleNew';
+import EScaleData from './scales/EScale'
+import FScaleData from './scales/FScale';
+import FSharpScale from './scales/F#Scale'
+import GScaleData from './scales/GScale';
+import GSharpScale from './scales/G#Scale';
+import AScaleData from './scales/AScale';
+import ASharpScale from './scales/A#Scale';
+import BScaleData from './scales/BScale';
+import CScaleData from './scales/CScale';
+import CSharpScale from './scales/C#Scale';
+import DScaleData from './scales/DScale';
+import DSharpScale from './scales/D#Scale';
 
 const marks = [
     {
