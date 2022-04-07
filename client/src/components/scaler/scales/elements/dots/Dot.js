957b19e4fbe8714cdfa6c8d0294afcb6086fa730
mdu830 <mdu830@yahoo.com>
2022-04-06 23:11:50 -0400

    small change to Dot

--- client/src/components/scaler/scales/elements/dots/Dot.js
+++ client/src/components/scaler/scales/elements/dots/Dot.js
@@ -7,8 +7,6 @@ export default function Dot(data) {
     const fade = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } })
     // const slideRight = useSpring({to: { x: 0 }, from: { x: -25 * data.data }})
  
-    // console.log(slide)
-
     return (
         <animated.div
             style={fade}
